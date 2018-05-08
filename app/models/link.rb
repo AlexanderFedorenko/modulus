d1d73f5986cc6ac3ebe3cee81d504d907af3fcdd
Alexander Fedorenko <alexander@fedorenko.im>
2018-05-08 20:38:52 +0300

    Improve randomness of short_url

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,20 +1,27 @@
 # frozen_string_literal: true
 
 require 'uri'
+require 'securerandom'
 
 class Link < ApplicationRecord
   validates :url, presence: true
 
   validate :url_is_looking_like_url
 
-  after_create :set_short_url
+  before_save :set_short_url
 
   def url_is_looking_like_url
     errors.add(:url, 'not valid') unless url&.match?(URI::DEFAULT_PARSER.regexp[:ABS_URI])
   end
 
   def set_short_url
-    hashids = Hashids.new('this is my salt', 6)
-    self.short_url = hashids.encode(id)
+    self.short_url = one_hundred_percent_true_random_string
+  end
+
+  private
+
+  def one_hundred_percent_true_random_string
+    hashids = Hashids.new('the salt', 6)
+    hashids.encode((SecureRandom.random_number(9e5) + 1e5).to_i)
   end
 end
